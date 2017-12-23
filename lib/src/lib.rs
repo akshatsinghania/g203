4cca567c631c4e399797f4cdf01927d79a9f22e6
Matjaž Drolc <mdrolc@gmail.com>
2017-12-24 00:01:41 +0100

    Added HID example code

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,13 +1,26 @@
 extern crate hidapi;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
 
 pub fn hello_from_lib() {
+    let api = hidapi::HidApi::new().unwrap();
+// Print out information about all connected devices
+    for device in &api.devices() {
+        println!("{:#?}", device);
+
+        let result = api.open_path(&device.path);
+        if result.is_err() {
+            println!("{:#?}", result.err());
+        } else {
+            let device = result.unwrap();
+
+            println!("{:#?}", device.check_error());
+
+            println!("{:#?}", device.get_manufacturer_string());
+            println!("{:#?}", device.check_error());
+
+            println!("{:#?}", device.get_product_string());
+            println!("{:#?}", device.check_error());
+        }
+    }
 }
 
